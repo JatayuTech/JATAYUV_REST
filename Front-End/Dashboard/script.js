17a036397f1f769896e08f60f932c7c349c00ec7
sandhya-s-cmd <godugusandhya654@gmail.com>
2025-05-28 22:25:23 +0530

    Dashboard
    Login history is removed from the dashboard.

--- Front-End/Dashboard/script.js
+++ Front-End/Dashboard/script.js
@@ -1,11 +1,17 @@
-document.querySelector('.button-link').addEventListener('click', (event) => {
-  // Optional: Prevent default navigation if you want to do something else first
-  // event.preventDefault();
-
-  console.log('Planning page link clicked!');
-});
-
 document.addEventListener("DOMContentLoaded", () => {
+  const logoutBtn = document.getElementById('logoutBtn');
+
+  if (logoutBtn) {
+    logoutBtn.addEventListener('click', (e) => {
+      e.preventDefault(); // Prevent the default anchor behavior
+      console.log('Logout clicked!');
+
+      alert('You have been logged out!');
+      window.location.href = 'index.html'; // or 'login.html' if you have one
+    });
+  }
+
+  
   const planningBtn = document.querySelector('.button-link');
   if (planningBtn) {
     planningBtn.addEventListener('click', () => {
